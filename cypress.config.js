6ad1e4afac9a793e558f02259102c32334fee2b1
Hizkia Andrew <hizkiaandrew@gmail.com>
2024-05-09 12:25:42 +0700

    fix chrome security

--- cypress.config.js
+++ cypress.config.js
@@ -7,4 +7,5 @@ module.exports = defineConfig({
       on("file:preprocessor", cucumber());
     },
   },
+  chromeWebSecurity: false
 });
--- cypress/e2e/cucumber/Pages/User/SemuaJasaPage.cy.js
+++ cypress/e2e/cucumber/Pages/User/SemuaJasaPage.cy.js
@@ -1,5 +1,4 @@
 import { SEMUA_JASA_URL } from "../../Utils/variable";
-import { URL_WHATSAPP_JASA_KONTAK_ADMIN } from "../../Utils/variable";
 
 class SemuaJasaPage {
   goToSemuaJasaWithUrl() {
@@ -11,42 +10,21 @@ class SemuaJasaPage {
   }
 
   clickKontakAdminMeaBtn() {
-        cy.window().then((win) => {
-          cy.stub(win, "open")
-            .callsFake((url, target) => {
-              expect(target).to.be.undefined;
-              // call the original `win.open` method
-              // but pass the `_self` argument
-              return win.open.wrappedMethod.call(win, url, "_self");
-            })
-            .as("open");
-        });
-            cy.get("[data-testid='btn-contact-admin-agencypricelist']").click();
+    cy.window().then((win) => {
+      cy.stub(win, "open")
+        .callsFake((url, target) => {
+          expect(target).to.be.undefined;
+          // call the original `win.open` method
+          // but pass the `_self` argument
+          return win.open.wrappedMethod.call(win, url, "_self");
+        })
+      .as("open");
+    });
+    cy.get("[data-testid='btn-contact-admin-agencypricelist']").click();
   }
 
   verifyWhatsappJasaOpenedOnNewTab() {
-    // cy.window().then(win => {
-    //     cy.stub(win, "open")
-    //     .callsFake((url, target) => {
-    //         expect(target).to.be.undefined;
-    //         // call the original `win.open` method
-    //         // but pass the `_self` argument
-    //         return win.open.wrappedMethod.call(win, url, "_self");
-    //     })
-    //     .as("open");
-    //     })
-    // cy.url().should("include", URL_WHATSAPP_JASA_KONTAK_ADMIN);
-  //  window.location.replace(URL_WHATSAPP_JASA_KONTAK_ADMIN);
-  //   cy.url().should("include", "whatsapp");
-    // cy.visit("https://staging.komunitasmea.com/jasa-kami");
-    // cy.window().then(win => {
-    //   cy.stub(win, "open", (url) => {
-    //     win.location.href = URL_WHATSAPP_JASA_KONTAK_ADMIN;
-    //   }).as("popup");
-    // });
-    // cy.get("[data-testid='btn-contact-admin-agencypricelist']").click();
-    // cy.url().should("include", "whatsapp");
-    // cy.get("h1").should("have.text", "Welcome to the-internet");
+    cy.url().should("include", "whatsapp");
   }
 }
 
